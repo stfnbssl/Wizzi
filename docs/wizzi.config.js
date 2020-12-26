c71d6163bb9f64be9417c769bbe0a9a8428c8017
stfnbssl <stfn.bssl@gmail.com>
2020-12-26 13:39:24 +0100

    docs 26 12 20 .3

---
+++ docs/wizzi.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+    wfjobName: "stfnbssl.github.io/wizzi/docs-job", 
+    wfjobPath: path.join(__dirname, '.wizzi', 'generate.wfjob.ittf'), 
+    destPath: path.join(__dirname),
+    plugins: [
+        './wizzi-core/dist/index.js', 
+        './wizzi-js/dist/index.js', 
+        './wizzi-web/dist/index.js', 
+    ], 
+    pluginsBaseFolder: path.join(__dirname, '..', 'packages'),
+    /*
+    plugins: [
+        'wizzi-core',
+        'wizzi-web',
+        'wizzi-js',
+    ],
+    */ 
+    schemas: [
+    ],
+    globalContext: {
+        isPackageDeploy: true,
+        isDevelopment: false,
+    },
+};
